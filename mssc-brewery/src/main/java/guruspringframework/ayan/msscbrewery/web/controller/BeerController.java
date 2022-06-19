f0d1372a5856ec87da8e16fee1cb2adecaa02ae0
amukh93 <mukherjeeayan952@gmail.com>
2022-06-19 15:12:59 +0530

    RestCOntroller modification

--- mssc-brewery/src/main/java/guruspringframework/ayan/msscbrewery/web/controller/BeerController.java
+++ mssc-brewery/src/main/java/guruspringframework/ayan/msscbrewery/web/controller/BeerController.java
@@ -1,10 +1,16 @@
 package guruspringframework.ayan.msscbrewery.web.controller;
 
+import java.util.UUID;
+
+import javax.websocket.server.PathParam;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import guruspringframework.ayan.msscbrewery.web.module.BeerDTO;
@@ -13,8 +19,8 @@ import guruspringframework.ayan.msscbrewery.web.module.BeerDTO;
 @RequestMapping("api/v1/beer")
 public class BeerController {
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<BeerDTO> getBeer(){
+	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE , value = {"/beerId"})
+	public ResponseEntity<BeerDTO> getBeer(@PathVariable("beerId") UUID beerID){
 		
 		return new ResponseEntity<BeerDTO>(BeerDTO.builder().build(), HttpStatus.OK);
 
